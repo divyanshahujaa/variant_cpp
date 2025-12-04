76876acf70054774c588ba52c9f44e35274f8458
Divyansh <divyansh9801@gmail.com>
2025-12-04 18:48:15 +0530

    Added = operator, is() and valid() functionality

--- bootlegVariant.h
+++ bootlegVariant.h
@@ -2,7 +2,7 @@
 #include <typeinfo>
 #include <utility>
 
-template  <typename T, typename... Args>
+template  <typename     T, typename... Args>
 struct variant_helper{
     static const size_t size = sizeof(T)>variant_helper<Args...>::size ? sizeof(T) : variant_helper<Args...>::size;
 
@@ -14,6 +14,14 @@ struct variant_helper{
             variant_helper<Args...>::destroy(id, data);
         }
     }
+
+    inline static void copy(size_t old_t, const void * old_v, void * new_v)
+    {
+        if (old_t == typeid(T).hash_code())
+        new (new_v) T(*reinterpret_cast<const T*>(old_v));
+        else
+        variant_helper<Args...>::copy(old_t, old_v, new_v);
+    }
 };
 
 template  <typename T>
@@ -24,6 +32,12 @@ struct variant_helper<T>{
         if(id==typeid(T).hash_code()){
             reinterpret_cast<T*>(data)->~T();
         }
+    }
+
+    inline static void copy(size_t old_t, const void * old_v, void * new_v)
+    {
+        if (old_t == typeid(T).hash_code())
+        new (new_v) T(*reinterpret_cast<const T*>(old_v));
     }
 };
 
@@ -45,11 +59,17 @@ struct variant{
     size_t type_id;
     raw_data<helper::size> data;
 
-    variant() : type_id(invalid_type()) { }
+    variant<T, Args...>() : type_id(invalid_type()) { }
 
     variant(variant<T, Args...>&& old) : type_id(old.type_id), data(old.data)
     {
         old.type_id = invalid_type();
+    }
+
+    variant(const variant<T, Args...>& old) : type_id(old.type_id)
+    {
+        // We need to do a "deep" copy, that is - invoke the copy constructor of the contained type.
+        helper::copy(old.type_id, &old.data, &data);
     }
 
     template<typename F, typename... Ts>
@@ -70,5 +90,30 @@ struct variant{
         else
         throw std::bad_cast();
     }
-};
 
+    variant<T, Args...>& operator= (variant<T, Args...>&& old){
+        data = old.data;
+        old.type_id = invalid_type();
+        return *this;
+    }
+
+    variant<T, Args...>& operator= (variant<T, Args...> old)
+    {
+        // Note the call-by-value above
+        std::swap(data, old.data);
+        std::swap(type_id, old.type_id);
+
+        return *this;
+    }
+
+    template<typename Tn>
+    bool is() {
+        return (type_id == typeid(Tn).hash_code());
+    }
+
+    bool valid() {
+       return (type_id != invalid_type());
+    }
+    
+    ~variant() { helper::destroy(type_id, &data); }
+};
